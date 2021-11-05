521d0d52c5df0589e9a470a8f39f77282e23a0f3
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-11-05 15:40:29 -0300

    Fade Effects

--- script_jquery.js
+++ script_jquery.js
@@ -1,9 +1,15 @@
 $(function(){
 	$('#first').click(function(){
 		$('p').css('color', 'blue');
+		$('p').fadeOut('slow');
+		$('p').delay(1000);
+		$('p').fideIn('slow');
 	});
 	$('#second').click(function(){
-		$('p').css('color', 'red')
+		$('p').css('background-color', 'red');
+		$('p').fadeOut('slow');
+		$('p').delay(1000);
+		$('p').fideIn('slow');
 	});
 });
 
