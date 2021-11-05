590311db13a437defb0420f08d83ee3ac0b941cd
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-11-05 11:46:32 -0300

    Logical operations

--- script.js
+++ script.js
@@ -4,4 +4,15 @@
  em várias linha de código*/
 
 var mensagem = "Olá mundo!";
+var a = 2;
+var b = 3;
+var soma = a+b;
+var sub = a-b;
+var mult = a*b;
+var div = a/b;
+
 alert (mensagem);
+alert (soma);
+alert (sub);
+alert (mult);
+alert (div);
