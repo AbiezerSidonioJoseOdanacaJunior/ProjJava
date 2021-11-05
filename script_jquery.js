1a9dacb6705f0eb222ffa9d6d10e4361e426a938
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-11-05 16:42:58 -0300

    recording messages

--- script_jquery.js
+++ script_jquery.js
@@ -1,15 +1,23 @@
 $(function(){
 	$('#first').click(function(){
 		$('p').css('color', 'blue');
-		$('p').fadeOut('slow');
+		$('p').fadeOut();
 		$('p').delay(1000);
-		$('p').fideIn('slow');
+		$('p').fideIn();
 	});
 	$('#second').click(function(){
 		$('p').css('background-color', 'red');
 		$('p').fadeOut('slow');
 		$('p').delay(1000);
 		$('p').fideIn('slow');
+	});
+	$('#save').click(function(){
+		$('p').css('background-color', 'blue');
+		$('#message').text("Cor alterado com sucesso!");
+		$('#message').css('border', '1px solid red');
+		$('#message').delay(3000);
+		$('#message').fadeOut('fast');
+		
 	});
 });
 
