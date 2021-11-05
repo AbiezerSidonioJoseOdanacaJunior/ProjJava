d24b39ac1380816667f9614def53e0efe391819e
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-11-05 12:10:51 -0300

    Comand else and else if

--- script.js
+++ script.js
@@ -6,12 +6,21 @@
 var mensagem = "Olá mundo!";
 var a = 2;
 var b = 3;
+var c = ("Olá mundo!");
 var soma = a+b;
 var sub = a-b;
 var mult = a*b;
 var div = a/b;
 
 //O modulo pega o resto da divisão nesse caso  vai imprimir o impar.
-if (b%2 == 1){
-	alert("Número ípmar")
+if (c%2 == 1){
+	alert("Número ípmar");
+}
+
+else if (c%2 == 0) {
+	alert ("Número par");
+}
+
+else {
+	alert ("Valor inválido");
 }
