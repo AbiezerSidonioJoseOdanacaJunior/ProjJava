4cd0dc9e1261db899aba9129e6a66166ebcd4e26
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-11-05 22:06:18 -0300

    Slide show

--- script_jquery.js
+++ script_jquery.js
@@ -20,5 +20,46 @@ $(function(){
 		delay(3000).fadeOut('fast');
 		
 	});
+
+	$('#l2').click(function(){
+		$('#i1').hide();
+		$('#i3').hide();
+		$('#i4').hide();
+		$('#i5').hide();
+		$('#i2').show();
+	});
+
+	$('#l3').click(function(){
+		$('#i1').hide();
+		$('#i2').hide();
+		$('#i4').hide();
+		$('#i5').hide();
+		$('#i3').show();
+	});
+
+
+	$('#l4').click(function(){
+		$('#i3').hide();
+		$('#i1').hide();
+		$('#i2').hide();
+		$('#i5').hide();
+		$('#i4').show();
+	});
+
+	$('#l5').click(function(){
+		$('#i4').hide();
+		$('#i1').hide();
+		$('#i2').hide();
+		$('#i3').hide();
+		$('#i5').show();
+	});
+
+	$('#l1').click(function(){
+		$('#i4').hide();
+		$('#i5').hide();
+		$('#i2').hide();
+		$('#i3').hide();
+		$('#i1').show();
+	});
 });
 
